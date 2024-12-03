287ab6ad11860f11c4532ca429944f3a4a43830d
zafar-m3n <thezafar.m3n@gmail.com>
2024-12-03 19:28:34 +0530

    Passport Setup

---
+++ config/passportSetup.js
@@ -0,0 +1,54 @@
+const passport = require("passport");
+const MicrosoftStrategy = require("passport-microsoft").Strategy;
+const dotenv = require("dotenv");
+const { User } = require("../models");
+dotenv.config();
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findByPk(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((err) => {
+      done(err, null);
+    });
+});
+
+passport.use(
+  new MicrosoftStrategy(
+    {
+      callbackURL: `${process.env.NODE_INTERNCONNECT_BASEURL}/api/v1/auth/microsoft/redirect`,
+      clientID: process.env.NODE_INTERNCONNECT_MICROSOFT_CLIENT_ID,
+      clientSecret: process.env.NODE_INTERNCONNECT_MICROSOFT_CLIENT_SECRET,
+      scope: ["openid", "profile", "email", "User.Read"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const currentUser = await User.findOne({
+          where: { microsoftId: profile.id },
+        });
+
+        if (currentUser) {
+          return done(null, currentUser);
+        } else {
+          const newUser = await User.create({
+            microsoftId: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            // profilePic: profile.photos
+            //   ? profile.photos[0].value
+            //   : "/images/profile.png",
+          });
+
+          return done(null, newUser);
+        }
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
--- models/User.js
+++ models/User.js
@@ -1,50 +1,56 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
 
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    microsoftId: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    profilePic: {
+      type: DataTypes.STRING,
+      defaultValue: "/images/profile.png",
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    isLecturer: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    notifications: {
+      type: DataTypes.JSON,
+      defaultValue: [],
+    },
+    seenNotifications: {
+      type: DataTypes.JSON,
+      defaultValue: [],
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  microsoftId: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  profilePic: {
-    type: DataTypes.STRING,
-    defaultValue: '/images/profile.png',
-  },
-  isAdmin: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-  isLecturer: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-  notifications: {
-    type: DataTypes.JSON,
-    defaultValue: [],
-  },
-  seenNotifications: {
-    type: DataTypes.JSON,
-    defaultValue: [],
-  },
-}, {
-  tableName: 'users',
-  createdAt: 'createdAt', 
-  updatedAt: 'updatedAt', 
-});
+  {
+    tableName: "users",
+    timestamps: true,
+  }
+);
 
 module.exports = User;
