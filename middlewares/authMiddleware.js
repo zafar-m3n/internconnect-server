364bd250110ab5a39129a70b9b3564cbc024491b
zafar-m3n <thezafar.m3n@gmail.com>
2024-12-03 19:32:24 +0530

    Auth Middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,28 @@
+const JWT = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.headers["authorization"].split(" ")[1];
+    JWT.verify(
+      token,
+      process.env.NODE_INTERNCONNECT_JWT_SECRET,
+      (err, decode) => {
+        if (err) {
+          return res.status(200).send({
+            message: "Authorization Failed",
+            success: false,
+          });
+        } else {
+          req.body.userId = decode.user;
+          next();
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(401).send({
+      message: "Authorization Failed",
+      success: false,
+    });
+  }
+};
